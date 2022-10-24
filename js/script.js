9fb00e548c60eb654cf6924826246a7ee528e1fc
Jakub Gluch <jakub.gluch92@gmail.com>
2022-10-24 17:59:10 +0200

    improved readability of JS , removed Photo button

--- js/script.js
+++ js/script.js
@@ -1,60 +1,57 @@
-console.log("Hello Neo")
+{
+    const welcome = () => {
+        console.log("Hello Neo...")
+    };
 
-let header__buttonPhotoElement = document.querySelector(".js-buttonPhoto");
-let header__themeButtonElement = document.querySelector(".js-themeButton")
-let main__imgElement = document.querySelector(".js-mainImg");
-let header_PhotoElement = document.querySelector(".js-headerPhoto")
-let bodyElement = document.querySelector(".js-body");
-let navElement = document.querySelector(".js-nav")
-let tableElement = document.querySelector(".js-table")
-let headerElement = document.querySelector(".js-header")
-let main__headerElement = document.querySelector(".js-mainHeader")
-let frontend__headerElement = document.querySelector(".js-frontendHeader")
-let hobby__headerElement = document.querySelector(".js-hobbyHeader")
-let header__buttonWordElement = document.querySelector(".js-buttonWord")
-let nav__linkSites = document.querySelectorAll(".js-linkSite")
-let hobby__linkElement = document.querySelector(".js-hobbyLink")
-let table__links = document.querySelectorAll(".js-tableLink")
-let footerElement = document.querySelector(".js-footer")
-let table__Cells = document.querySelectorAll(".js-tableCell")
-let table__Headers = document.querySelectorAll(".js-tableHeader")
+    const toggleBackground = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const navElement = document.querySelector(".js-nav");
+        const headerElement = document.querySelector(".js-header");
+        const main__headerElement = document.querySelector(".js-mainHeader");
+        const frontend__headerElement = document.querySelector(".js-frontendHeader");
+        const hobby__headerElement = document.querySelector(".js-hobbyHeader");
+        const hobby__linkElement = document.querySelector(".js-hobbyLink");
+        const footerElement = document.querySelector(".js-footer");
+        const table__links = document.querySelectorAll(".js-tableLink");
+        const nav__linkSites = document.querySelectorAll(".js-linkSite");
+        const table__Cells = document.querySelectorAll(".js-tableCell");
+        const table__Headers = document.querySelectorAll(".js-tableHeader");
+        const header__buttonWordElement = document.querySelector(".js-buttonWord");
 
-header__buttonPhotoElement.addEventListener("click", () => {
-    main__imgElement.remove(".main__img");
+        bodyElement.classList.toggle("body--darkTheme");
+        navElement.classList.toggle("nav--darkTheme");
+        headerElement.classList.toggle("header--darkTheme");
+        main__headerElement.classList.toggle("main--darkTheme");
+        frontend__headerElement.classList.toggle("frontend--darkTheme");
+        hobby__headerElement.classList.toggle("hobby--darkTheme");
+        hobby__linkElement.classList.toggle("hobby__link--darkTheme");
+        footerElement.classList.toggle("footer--darkTheme");
+        table__links.forEach((table__link) => {
+            table__link.classList.toggle("table__links--darkTheme")
 
-});
+        });
+        nav__linkSites.forEach((nav__linkSite) => {
+            nav__linkSite.classList.toggle("nav_linkSite--darkTheme")
 
-header__themeButtonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--darkTheme");
-    navElement.classList.toggle("nav--darkTheme");
-    headerElement.classList.toggle("header--darkTheme");
-    main__headerElement.classList.toggle("main--darkTheme");
-    frontend__headerElement.classList.toggle("frontend--darkTheme");
-    hobby__headerElement.classList.toggle("hobby--darkTheme");
-    hobby__linkElement.classList.toggle("hobby__link--darkTheme");
-    footerElement.classList.toggle("footer--darkTheme");
+        });
+        table__Cells.forEach((table__Cell) => {
+            table__Cell.classList.toggle("table__cell--darkTheme")
 
-    table__links.forEach((table__link) => {
-        table__link.classList.toggle("table__links--darkTheme")
+        });
+        table__Headers.forEach((table__Header) => {
+            table__Header.classList.toggle("table__header--darkTheme")
 
-    });
+        });
+        header__buttonWordElement.innerText = bodyElement.classList.contains("body--darkTheme") ? "jasny" : "ciemny"
+    };
 
-    nav__linkSites.forEach((nav__linkSite) => {
-        nav__linkSite.classList.toggle("nav_linkSite--darkTheme")
+    const init = () => {
+        const header__themeButtonElement = document.querySelector(".js-themeButton");
+        header__themeButtonElement.addEventListener("click", toggleBackground);
 
-    });
+        welcome();
+    };
 
-    table__Cells.forEach((table__Cell) => {
-        table__Cell.classList.toggle("table__cell--darkTheme")
+    init();
 
-    });
-
-    table__Headers.forEach((table__Header) => {
-        table__Header.classList.toggle("table__header--darkTheme")
-
-    });
-
-
-    header__buttonWordElement.innerText = bodyElement.classList.contains("body--darkTheme") ? "jasny" : "ciemny"
-
-});
+}
